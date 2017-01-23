6568f0ba1a11ec4fcb9ecb2cf633ae4a500a3842
Omid Khataee <maverickxm2@live.ca>
2017-01-23 10:41:25 -0500

    Links to the HTML pages and completed HTML pages

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ let express = require('express')
 // created the express???
 let app = express()
 
+// unknown as of right now
+//var router = express.Router();
+
+// setting the absolute value of the path + the folder where HTML files are kept
+let path = __dirname + '/views/'
+
 // set port to 3000
 const port = 3000;
 
@@ -15,17 +21,17 @@ console.log(`Server listening at port: ${port}`)
 
 // About page
 app.get('/about', function(req, res){
-    res.send('About')
+ res.sendFile(path + 'about.html')
 })
 
 // Contage page
 app.get('/contact', function(req,res){
-    res.send('Contact')
+ res.sendFile(path + 'contact.html')
 })
 
 // Landing Page
 app.get('/', function(req, res){
-    res.send('Hello World')
+    res.sendFile(path + 'index.html')
 })
 
 
